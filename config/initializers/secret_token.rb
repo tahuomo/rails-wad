fad8485c338ef07958aaa0c94c5422e4e64173a0
saasbook <saasbook@saasbook.(none)>
2013-11-07 07:22:34 -0800

    skeleton generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratebeer::Application.config.secret_token = 'a9667e2fc4bc9b036a7ce6a0ebe21ca14f0c02ea380d7aab4f8936c1d7a2551279047a70cb4c24da1f472f0ae292a1ccff58beb4a45d9740ff776169b1746bfa'
